b27125aaabd5326357e63ca809883e43df9fa265
LAPTOP-2C7EJNV9\User01 <raya.sabari@sigma-tech.co.id>
2023-01-10 15:33:23 +0700

    Install Storybook

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <div style="margin-top: 40px;">
+    <Button label="Get Started" :primary="true" />
+  </div>
+  <div style="margin-top: 10px;">
+    <Button label="Learn More" size="small" />
+  </div>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Button from './stories/Button.vue'
+import Header from './stories/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Button,
+    Header
   }
 }
 </script>
 
 <style>
+body {
+  margin: auto;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
